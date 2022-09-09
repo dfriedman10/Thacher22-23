9a497940f86363dfd1daa6d5154c03b97dfba6b5
dfriedman <dfriedman@10.0.13.175>
2022-09-09 11:12:49 -0700

    MyNumber example

---
+++ Java/src/MyNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MyNumber {
+	
+	Scanner in = new Scanner(System.in);
+	
+	int number;
+	
+	public void setNumber() {
+		
+		number = in.nextInt();
+	}
+	
+	public void printNumber() {
+		
+		System.out.println(number);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

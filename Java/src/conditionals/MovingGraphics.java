e1b59da58a08a772b2590821a5b2987863d095cf
dfriedman <dfriedman@10.0.13.175>
2022-10-03 12:58:58 -0700

    graphics updates

--- Java/src/conditionals/MovingGraphics.java
+++ Java/src/conditionals/MovingGraphics.java
@@ -6,15 +6,22 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GraphicsAnimation {
+public class MovingGraphics {
+	
+	int x = 0, y = 300;
 	
 	public void draw(Graphics g) {
 		
 		g.setColor( new Color(255, 0, 0)   );
 		
-		g.fillPolygon(new int[] {100, 3, 300}, new int[] {100, 200, 300}, 3);
+//		g.fillPolygon(new int[] {100, 300, 300}, new int[] {100, 200, 300}, 3);
 		
+		g.fillRect(x, y, 50, 50);
+	}
+	
+	public void move() {
 		
+		x += 8;
 	}
 	
 
@@ -25,6 +32,8 @@ public class GraphicsAnimation {
 		window.setSize(600, 600);
 		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		window.setBackground(Color.blue);
 		
 		
 		JPanel canvas = new JPanel() {
@@ -37,11 +46,25 @@ public class GraphicsAnimation {
 		
 		window.add(canvas);
 		window.setVisible(true);
+		
+		while (true) {
+			
+			move();
+			canvas.repaint();
+			
+			try {
+				Thread.sleep(35);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
 	}
 
 	public static void main(String[] args) {
 
-		GraphicsAnimation test = new GraphicsAnimation();
+		MovingGraphics test = new MovingGraphics();
 		test.startGraphics();
 		
 	}

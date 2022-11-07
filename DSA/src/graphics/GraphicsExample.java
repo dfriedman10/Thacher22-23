055c555c90556bf07363483768c0e891ce72b650
dfriedman <dfriedman@10.0.13.175>
2022-11-07 13:28:03 -0800

    update

--- DSA/src/graphics/GraphicsExample.java
+++ DSA/src/graphics/GraphicsExample.java
@@ -1,20 +1,25 @@
 package graphics;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GraphicsExample {
 	
-	final int WIDTH = 600, HEIGHT = 600;
+	final int WIDTH = 600, HEIGHT = 600, BUTTONHEIGHT = 50;
 	
 	public void draw(Graphics g) {
 		
-		g.setColor(Color.red);
+		g.setColor(new Color(100, 50, 25));
 		g.fillRect(100, 100, 50, 50);
 		
+		g.fillPolygon(new int[] {400, 200, 300}, 
+				new int[] {100, 100, 200}, 3);
 	}
 
 	public void startGraphics() {
@@ -35,7 +40,17 @@ public class GraphicsExample {
 			}
 		};
 		
-		frame.add(canvas);
+		canvas.setSize(new Dimension(WIDTH, HEIGHT - BUTTONHEIGHT));
+		
+		JPanel container = new JPanel();
+		
+		JButton button = new JButton("Draw Circle");
+		
+		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+		container.add(canvas);
+		container.add(button);
+		
+		frame.add(container);
 		
 		frame.setVisible(true);
 

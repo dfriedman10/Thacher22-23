05b15f7d507f2ff34d620eeda2445945ca72007e
dfriedman <dfriedman@10.0.13.175>
2022-11-15 12:58:49 -0800

    text example

---
+++ DSA/src/graphics/TextandButtons.java
@@ -0,0 +1,78 @@
+package graphics;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class TextandButtons {
+	
+	final int WIDTH = 600, HEIGHT = 600, BUTTONHEIGHT = 50;
+
+	public void startGraphics() {
+		
+		JFrame frame = new JFrame("Graphics Example");
+		
+		frame.setSize(WIDTH, HEIGHT);
+		
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		
+		JTextArea textBox = new JTextArea();
+		
+		textBox.setSize(new Dimension(WIDTH, HEIGHT - BUTTONHEIGHT));
+		
+		textBox.setText("Welcome to ____");
+		
+		textBox.setEditable(false);
+		
+		JPanel container = new JPanel();
+		
+		JButton button = new JButton("Say Hi");
+		
+		button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				textBox.setText(textBox.getText() + "\nHI");
+				
+			}
+			
+		});
+		
+		JButton button2 = new JButton("button 2");
+		
+		JPanel buttonContainer = new JPanel();
+		
+		
+		
+		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+		container.add(textBox);
+		buttonContainer.add(button);
+		buttonContainer.add(button2);
+		container.add(buttonContainer);
+		
+		frame.add(container);
+		
+		frame.setVisible(true);
+
+	}
+	
+	public static void main(String[] args) {
+		new TextandButtons().startGraphics();
+	}
+	
+	
+	
+}

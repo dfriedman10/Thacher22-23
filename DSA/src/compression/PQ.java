6f9b0bceca46826c312d5a9eb564381a346da22b
dfriedman <dfriedman@10.0.13.175>
2023-01-24 13:39:45 -0800

    priority queue

---
+++ DSA/src/compression/PQ.java
@@ -0,0 +1,41 @@
+package compression;
+import java.util.ArrayList;
+
+public class PQ<T> {
+
+	private ArrayList<Node> queue = new ArrayList<Node>();
+	
+	class Node {
+		T info;
+		int priority;
+		
+		public Node(T info, int p) {
+			this.info = info;
+			priority = p;
+		}
+		
+		public String toString() {
+			return info.toString();
+		}
+	}
+	
+	public void add(T data, int p) {
+		
+	}
+	
+	// takes the last item, assuming our pq 
+	// is in descending order
+	public T pop() {
+		
+		return queue.remove(queue.size() - 1).info;
+	}
+	
+	public String toString() {
+		return queue.toString();
+	}
+	
+	public int size() {
+		return queue.size();
+	}
+	
+}

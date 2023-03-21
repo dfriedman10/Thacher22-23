74ec415f2f5288d3b55197163476a504d1bbd326
dfriedman <dfriedman@10.0.13.175>
2023-03-21 10:56:56 -0700

    student update

--- Java/src/objects/Student.java
+++ Java/src/objects/Student.java
@@ -7,11 +7,17 @@ public class Student {
 	int age;
 	String from;
 	
-	public Student(String n, int g, int a, String f) {
+	public Student(String n, int a, int g, String f) {
 		name = n; 
 		grade = g;
 		age = a; 
 		from = f;
+	}
+	
+	public String toString() {
+		
+		return "Name: " + name + ", age: " + age + ", grade: " + 
+		grade + ", from: " + from;
 	}
 	
 	public void getOlder() {
@@ -28,14 +34,28 @@ public class Student {
 				name + ", I'm from " + from) ;
 	}
 	
+	public void inSameGrade(Student other) {
+		
+		if (other.grade == grade) {
+			System.out.println("same grade!");
+		}
+		else {
+			System.out.println("nope");
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		Student george = new Student("George", 15, 10, "New York");
 		
 		Student lucien = new Student("Lucien", 18, 12, "Seattle");
 		
-		george.move("SF");
+		george.getOlder();
+		george.getOlder();
 		
-		george.sayHi();
+		
+		george.inSameGrade(lucien);
+		
+		
 	}
 }

7b34fff882497d04e70663af8a53206ca0574cba
dfriedman <dfriedman@10.0.13.175>
2023-04-04 11:13:08 -0700

    private inheritance example

--- Java/src/objects/SoccerPlayer.java
+++ Java/src/objects/SoccerPlayer.java
@@ -2,26 +2,49 @@ package objects;
 
 public class SoccerPlayer extends Athlete {
 	
-	int goals;
-
+	int kickStrength;
 	
-	public SoccerPlayer(String name, boolean pro, int goals) {
-		super("soccer", name, pro);
+	public SoccerPlayer(String name, 
+			boolean pro, int vert, int numSponsors, int kickStrength) {
 		
-		this.goals = goals;
+		super("soccer", name, pro, vert, numSponsors);
+		
+		this.kickStrength = kickStrength;
+	}
+
+	public void shoot() {
+		if (kickStrength > 5 && super.getVert() > 20) {
+			System.out.println("GOAAAAAL");
+		}
+		else {
+			System.out.println("weak");
+		}
 	}
 	
-	public void score() {
-		goals++;
-		super.timePracticed += 100;
+
+	public void train() {
+		
+		super.train();
+		
+		kickStrength ++;
+
 	}
 	
 	public String toString() {
 		
-		return super.toString() + " goals: " + goals;
-		
+		return super.toString() + " strength: " + kickStrength;
 	}
-
+	
+	public static void main(String[] args) {
+		
+		
+		
+		SoccerPlayer messi = new SoccerPlayer("Messi", true, 35, 100, 5);
+		
+		messi.shoot();
+		messi.train();
+		System.out.println(messi);
+	}
 	
 	
 	
